4946cb14d1edd5a0368d8a1170433f73a1b592a8
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-05-26 09:09:38 -0500

    feat(VenueRepository): add repository to manage Venue information

--- src/main/java/com/chat/sbhu/repository/VenueRepository.java
+++ src/main/java/com/chat/sbhu/repository/VenueRepository.java
@@ -1,4 +1,43 @@
 package com.chat.sbhu.repository;
 
+import com.chat.sbhu.models.Venue;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
 public class VenueRepository {
+
+    private final Map<UUID, Venue> database = new HashMap<>();
+
+    public List<Venue> getAll () {
+
+        if (database.isEmpty()){
+
+            return null;
+
+        }
+
+        return new ArrayList<>(database.values());
+
+    }
+
+    public Venue getById (UUID id){
+
+        return database.get(id);
+
+    }
+
+    public boolean add (Venue venue){
+
+        return database.put(venue.getId(), venue) != null;
+
+    }
+
+    public boolean delete (UUID id){
+
+        return database.remove(id) != null;
+
+    }
+
 }
