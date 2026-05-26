dca72de1fd8605457732e8429c971e2aecc4ae48
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-05-26 09:03:15 -0500

    feat(EventRepository): add memory database for manage the Event information

--- src/main/java/com/chat/sbhu/repository/EventRepository.java
+++ src/main/java/com/chat/sbhu/repository/EventRepository.java
@@ -1,4 +1,43 @@
 package com.chat.sbhu.repository;
 
+import com.chat.sbhu.models.Event;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
 public class EventRepository {
+
+    private final Map<UUID, Event> database = new HashMap<>();
+
+    public List<Event> getAll () {
+
+        if (database.isEmpty()){
+
+            return null;
+
+        }
+
+        return new ArrayList<>(database.values());
+
+    }
+
+    public Event getById (UUID id){
+
+        return  database.get(id);
+
+    }
+
+    public boolean add (Event event) {
+
+        return database.put(event.getId(), event) != null;
+
+    }
+
+    public boolean delete (UUID id){
+
+        return database.remove(id) != null;
+
+    }
+
 }
