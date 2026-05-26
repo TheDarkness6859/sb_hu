511495afab5c22484a1378a218992cf4660b5105
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-05-26 08:20:21 -0500

    feat(Event): add Event model with its attributes

--- src/main/java/com/chat/sbhu/models/Event.java
+++ src/main/java/com/chat/sbhu/models/Event.java
@@ -1,4 +1,19 @@
 package com.chat.sbhu.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Event {
+
+    private UUID id;
+    private String name;
+    private Venue venue;
+    private String type;
+
 }
