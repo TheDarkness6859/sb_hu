5e31715b30a1d391ee877c9f19328ef5baf8bc74
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-05-26 09:26:46 -0500

    feat(VenueService): add validations and connection with repository

--- src/main/java/com/chat/sbhu/service/VenueService.java
+++ src/main/java/com/chat/sbhu/service/VenueService.java
@@ -1,4 +1,57 @@
 package com.chat.sbhu.service;
 
+import com.chat.sbhu.models.Venue;
+import com.chat.sbhu.repository.VenueRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class VenueService {
+
+    private final VenueRepository repository;
+
+    public VenueService (VenueRepository repository) {
+
+        this.repository = repository;
+
+    }
+
+    public Venue getById (UUID id){
+
+        if (id != null){
+
+            return repository.getById(id);
+
+        }
+
+        return null;
+
+    }
+
+    public List<Venue> getAll () {
+
+        return repository.getAll();
+
+    }
+
+
+    public boolean add (Venue event){
+
+        if (event != null){
+
+            return repository.add(event);
+
+        }
+
+        return false;
+
+    }
+
+    public boolean delete (UUID id){
+
+        return repository.delete(id);
+
+    }
 }
