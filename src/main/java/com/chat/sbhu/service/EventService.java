e83a819040f78a788483e53457c72d2b896699f6
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-05-26 09:24:48 -0500

    feat(EventService): add validations and connection with repository

--- src/main/java/com/chat/sbhu/service/EventService.java
+++ src/main/java/com/chat/sbhu/service/EventService.java
@@ -1,4 +1,58 @@
 package com.chat.sbhu.service;
 
+import com.chat.sbhu.models.Event;
+import com.chat.sbhu.repository.EventRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class EventService {
+
+    private final EventRepository repository;
+
+    public EventService (EventRepository repository) {
+
+        this.repository = repository;
+
+    }
+
+    public Event getById (UUID id){
+
+        if (id != null){
+
+            return repository.getById(id);
+
+        }
+
+        return null;
+
+    }
+
+    public List<Event> getAll () {
+
+        return repository.getAll();
+
+    }
+
+
+    public boolean add (Event event){
+
+        if (event != null){
+
+            return repository.add(event);
+
+        }
+
+        return false;
+
+    }
+
+    public boolean delete (UUID id){
+
+        return repository.delete(id);
+
+    }
+
 }
