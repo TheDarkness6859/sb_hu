581ab86221d595f7bf634a98c20727cb0cc0f3a8
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-05-26 08:21:03 -0500

    feat(Venue): add Venue model with its attributes

--- src/main/java/com/chat/sbhu/models/Venue.java
+++ src/main/java/com/chat/sbhu/models/Venue.java
@@ -1,4 +1,20 @@
 package com.chat.sbhu.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Venue {
+
+    private UUID id;
+    private String name;
+    private String address;
+    private Event event;
+    private Integer capacity;
+
 }
