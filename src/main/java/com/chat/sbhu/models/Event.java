09b6cafb925fd4799a9f0eb23fa07d5bf0ddbdeb
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-05-26 21:43:08 -0500

    refactor(Event): update POJO to Entity

--- src/main/java/com/chat/sbhu/models/Event.java
+++ src/main/java/com/chat/sbhu/models/Event.java
@@ -1,19 +1,31 @@
 package com.chat.sbhu.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.UUID;
 
-@Data
+@Entity
+@Table(name = "event")
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Event {
 
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(nullable = false, length = 100)
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "venue_id", referencedColumnName = "id", nullable = false)
     private Venue venue;
+
+    @Column(nullable = false, length = 30)
     private String type;
 
 }
