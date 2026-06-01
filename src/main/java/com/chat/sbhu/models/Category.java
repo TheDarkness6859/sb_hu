8dc040d43d770c268648569eb4e7aea1cf1f5c1a
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-06-01 07:55:19 -0500

    feat(Category): add Category entity with its attributes

---
+++ src/main/java/com/chat/sbhu/models/Category.java
@@ -0,0 +1,27 @@
+package com.chat.sbhu.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+}
