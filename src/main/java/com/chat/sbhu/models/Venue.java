79ae6bc5e3f9390974bf7a940051f64ea3c11b0a
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-06-01 07:53:45 -0500

    feat(Venue): add city column to use City enum

--- src/main/java/com/chat/sbhu/models/Venue.java
+++ src/main/java/com/chat/sbhu/models/Venue.java
@@ -1,5 +1,6 @@
 package com.chat.sbhu.models;
 
+import com.chat.sbhu.models.enums.City;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -20,6 +21,10 @@ public class Venue {
 
     @Column(nullable = false, length = 100)
     private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private City city;
 
     @Column(nullable = false, length = 40)
     private String address;
