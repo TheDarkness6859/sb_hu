7bc67ce21b80e8bbacdf7954488f9f6d961b0cb0
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-06-02 11:48:22 -0500

    feat(VenueDto): add new atrribute to VenueDto

--- src/main/java/com/chat/sbhu/dto/VenueDto.java
+++ src/main/java/com/chat/sbhu/dto/VenueDto.java
@@ -1,8 +1,9 @@
 package com.chat.sbhu.dto;
 
 import com.chat.sbhu.models.Event;
+import com.chat.sbhu.models.enums.City;
 
 import java.util.UUID;
 
-public record VenueDto (UUID id, String name, String address, Event event, Integer capacity){
+public record VenueDto (UUID id, String name, String address, Event event, Integer capacity, City city){
 }
