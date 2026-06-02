7aa0beaabb9be454a9a647070dbaf369ff7e40af
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-06-02 11:45:48 -0500

    refactor(VenueRepository): modify clase to make a interface

--- src/main/java/com/chat/sbhu/repository/VenueRepository.java
+++ src/main/java/com/chat/sbhu/repository/VenueRepository.java
@@ -1,59 +1,21 @@
 package com.chat.sbhu.repository;
 
 import com.chat.sbhu.models.Venue;
-import com.chat.sbhu.repository.persistance.VenueDatabase;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.*;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public class VenueRepository {
+public interface VenueRepository extends JpaRepository<Venue, UUID> {
 
-    private final VenueDatabase dataBase;
+    Optional<Venue> getByName(String name);
 
-    public VenueRepository (VenueDatabase dataBase){
-        this.dataBase = dataBase;
-    }
-
-    public Page<Venue> getAllPaginated (Pageable pageable) {
-
-        return dataBase.findAll(pageable);
-
-    }
-
-    public Venue getById (UUID id){
-
-        return dataBase.getReferenceById(id);
-
-    }
-
-    public Venue getByName (String name){
-
-        return dataBase.findAll()
-                .stream()
-                .filter(venue -> name.equalsIgnoreCase(venue.getName()))
-                .findFirst()
-                .orElse(null)
-        ;
-
-    }
-
-    public boolean add (Venue venue){
-
-        dataBase.save(venue);
-
-        return true;
-
-    }
-
-    public boolean delete (UUID id){
-
-       dataBase.deleteById(id);
-
-       return true;
-
-    }
+    @Query("SELECT v FROM Venue v")
+    Page<Venue> getAllPaginated(Pageable pageable);
 
 }
