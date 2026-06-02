1c2682eb86b54613cc12fecd3e21251249893787
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-06-02 12:06:40 -0500

    refactor(WebEventController): modify to use correctly the slice and need requests

--- src/main/java/com/chat/sbhu/controllers/WebEventController.java
+++ src/main/java/com/chat/sbhu/controllers/WebEventController.java
@@ -1,16 +1,19 @@
 package com.chat.sbhu.controllers;
 
 import com.chat.sbhu.dto.EventDto;
+import com.chat.sbhu.models.Category;
 import com.chat.sbhu.models.Event;
 import com.chat.sbhu.models.Venue;
+import com.chat.sbhu.models.enums.City;
+import com.chat.sbhu.service.CategoryService;
 import com.chat.sbhu.service.EventService;
 import com.chat.sbhu.service.VenueService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/admin/events")
@@ -18,29 +21,27 @@ public class WebEventController {
 
     private final EventService service;
     private final VenueService venueService;
+    private final CategoryService categoryService;
 
-    public WebEventController (EventService service, VenueService venueService){
+    public WebEventController (EventService service, VenueService venueService, CategoryService categoryService){
         this.service = service;
         this.venueService = venueService;
+        this.categoryService = categoryService;
     }
 
     @GetMapping
     public String getEvents (
-            @PageableDefault(page = 0, size = 10 ,sort = "name") Pageable pageable,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
             Model model){
 
-        Page<Event> eventsPage = service.getAll(pageable);
+        Slice<EventDto> eventSlice = service.getCatalog(page, size);
 
-        model.addAttribute("events", eventsPage.getContent());
-
-        model.addAttribute("currentPage", eventsPage.getNumber());
-        model.addAttribute("totalPages", eventsPage.getTotalPages());
-        model.addAttribute("totalItems", eventsPage.getTotalElements());
-
-        if (pageable.getSort().isSorted()) {
-            model.addAttribute("sortField", pageable.getSort().iterator().next().getProperty());
-            model.addAttribute("sortDir", pageable.getSort().iterator().next().getDirection().name().toLowerCase());
-        }
+        model.addAttribute("events", eventSlice.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("slice", eventSlice);
+        model.addAttribute("cities", City.values());
+        model.addAttribute("allCategories", categoryService.getAll());
 
         return "admin";
 
@@ -50,13 +51,13 @@ public class WebEventController {
     public String create (
             @RequestParam("name") String name,
             @RequestParam("venue.name") String venueName,
-            @RequestParam("type") String type
+            @RequestParam("category") Set<Category> category
             ){
 
         Event event = new Event();
 
         event.setName(name);
-        event.setType(type);
+        event.setCategories(category);
 
         if (venueName != null && !venueName.trim().isEmpty()){
 
