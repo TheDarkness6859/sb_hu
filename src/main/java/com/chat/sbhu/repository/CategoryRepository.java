5c8ee4c7752eaf32e630023789cadc3353a8d13b
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-06-02 11:47:41 -0500

    feat(CategoryRepository): add new interface to manage the persistance

---
+++ src/main/java/com/chat/sbhu/repository/CategoryRepository.java
@@ -0,0 +1,15 @@
+package com.chat.sbhu.repository;
+
+import com.chat.sbhu.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+    Optional<Category> getByName(String name);
+
+}
